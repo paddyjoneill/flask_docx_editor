7b1251e1e649debe897e40b563e2d3d06c828565
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-04-27 14:48:50 +0100

    basics working

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 file = ''
 document = ''
 placeholders = {}
+replacements = {
+    "company": "Shell",
+    "field": "Auk"
+}
+
 
 @app.route('/')
 def index():
@@ -34,9 +39,9 @@ def load_template():
     if request.method == 'POST':
         template = request.files['file']
         document = Document(template)
-        string = document.paragraphs[0].text
+        filename = request.files['file'].filename
         document_change()
-        return render_template('load_template.html', file=string)
+        return render_template('load_template.html', file=filename)
     return render_template('load_template.html')
 
 @app.route('/download')
@@ -45,11 +50,29 @@ def download_file():
     return send_file(path, as_attachment=True)
 
 
+@app.route('/addreplacement', methods=['POST', 'GET'])
+def add_replacement():
+    if request.method == 'POST':
+        replacements[request.form['key']] = request.form['value'] 
+    return render_template('addreplacement.html', replacements=replacements)
+
+@app.route('/update/<key>')
+def update(key):
+    return render_template('addreplacement.html', replacements=replacements, keytoupdate=key)
+
+@app.route('/delete/<key>')
+def delete(key):
+    del replacements[key]
+    return render_template('addreplacement.html', replacements=replacements)
+
+
+
 def document_change():
     document = Document('test_file.docx')
     for paragraph in document.paragraphs:
         string = paragraph.text
-        string = string.replace('#name', 'Paddy')
+        for replacement in replacements:
+            string = string.replace('#' + replacement, replacements[replacement])
         paragraph.text = string
     document.save('new_file.docx')
     return document
